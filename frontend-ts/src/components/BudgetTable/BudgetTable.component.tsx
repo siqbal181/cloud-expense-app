a0c652981d9abddf8e292fd5a4f04da966d91f9c
Sidra Iqbal <sidra1360@gmail.com>
2024-01-11 11:45:02 +0000

    chore: Flask setup

--- frontend-ts/src/components/BudgetTable/BudgetTable.component.tsx
+++ frontend-ts/src/components/BudgetTable/BudgetTable.component.tsx
@@ -1,16 +1,14 @@
-import React, { FC } from 'react';
-import { BudgetTableProps } from './types';
+import React, { FC } from "react";
+import { BudgetTableProps } from "./types";
 import Paper from "@mui/material/Paper";
-
+import { Typography } from "@mui/material";
 
 export const BudgetTable: FC<BudgetTableProps> = () => {
-
   return (
     <>
       <Paper elevation={1} style={{ padding: 20, maxWidth: 500 }}>
-
-
-    </Paper>
+        <Typography variant="h6">Your Monthly Budgets</Typography>
+      </Paper>
     </>
-  )
-}
+  );
+};
